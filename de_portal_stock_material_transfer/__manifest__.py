ca58fe801a5f37c9ec8c05b701f3378e909ea87e
farhan <farhan@dynexcel.com>
2022-01-23 23:13:49 +0000

    update stock transfer

--- de_portal_stock_material_transfer/__manifest__.py
+++ de_portal_stock_material_transfer/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Stock',
-    'version': '14.0.0.1',
+    'version': '14.0.0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base','de_stock_material_transfer'],
