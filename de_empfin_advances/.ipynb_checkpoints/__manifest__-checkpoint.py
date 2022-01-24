16f2a0d7ab9ae28e57698c5da36e0cf6757d7032
farhan <farhan@dynexcel.com>
2022-01-24 06:36:15 +0000

    update advances

--- de_empfin_advances/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_empfin_advances/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Employee Advances',
-    'version': '14.0.2.6',
+    'version': '14.0.2.7',
     'summary': 'Employee Advance Request',
     'description': """
         Helps you to manage Advance Salary Request of your company's staff.
