ba1dd505048f229e232a6398bf8129cbdde4f2c9
farhan <farhan@dynexcel.com>
2022-02-08 18:14:16 +0000

    update currency

--- de_secondary_currency/models/.ipynb_checkpoints/account_move-checkpoint.py
+++ de_secondary_currency/models/.ipynb_checkpoints/account_move-checkpoint.py
@@ -18,23 +18,9 @@ class AccountMove(models.Model):
     _inherit = "account.move"
     
     company_currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
-    total_base_signed = fields.Monetary(string='Total base.Curr.', readonly=True, compute='_compute_all_currency_conversion_amount', currency_field='company_currency_id')
+    total_base_signed = fields.Monetary(string='Total base.Curr.', readonly=True, compute='_compute_all_currency_conversion_amount', currency_field='company_currency_id', store=True)
     
-    @api.depends(
-        'line_ids.matched_debit_ids.debit_move_id.move_id.payment_id.is_matched',
-        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual',
-        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.payment_id.is_matched',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency',
-        'line_ids.debit',
-        'line_ids.credit',
-        'line_ids.currency_id',
-        'line_ids.amount_currency',
-        'line_ids.amount_residual',
-        'line_ids.amount_residual_currency',
-        'line_ids.payment_id.state',
-        'line_ids.full_reconcile_id')
+    @api.depends('amount_total_signed',)
     def _compute_all_currency_conversion_amount(self):
         for move in self:
             total_base_signed = 0.0
